fe61b32adebc573ec55662de29a4556f1bf7ba8e
Er4ld0 <junior-eraldo2011@hotmail.com>
2023-12-06 23:51:20 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,131 @@
+function showDesc1() {
+    if (task__desc_1.style.display == 'none') {
+        task__desc_1.style.display = 'block'
+    } else {
+        task__desc_1.style.display = 'none'
+    }
+
+
+
+    if (arrow1.style.rotate == '-180deg') {
+        arrow1.style.rotate = '0deg'
+    } else {
+        arrow1.style.rotate = '-180deg'
+    }
+
+    if (arrow1.style.transition == '0.5s') {
+        arrow1.style.transition = '0'
+    } else {
+        arrow1.style.transition = '0.5s'
+    }
+}
+
+function showDesc2() {
+    if (task__desc_2.style.display == 'none') {
+        task__desc_2.style.display = 'block'
+    } else {
+        task__desc_2.style.display = 'none'
+    }
+
+
+
+    if (arrow2.style.rotate == '-180deg') {
+        arrow2.style.rotate = '0deg'
+    } else {
+        arrow2.style.rotate = '-180deg'
+    }
+
+    if (arrow2.style.transition == '0.5s') {
+        arrow2.style.transition = '0'
+    } else {
+        arrow2.style.transition = '0.5s'
+    }
+}
+
+function showDesc3() {
+    if (task__desc_3.style.display == 'none') {
+        task__desc_3.style.display = 'block'
+    } else {
+        task__desc_3.style.display = 'none'
+    }
+
+
+
+    if (arrow3.style.rotate == '-180deg') {
+        arrow3.style.rotate = '0deg'
+    } else {
+        arrow3.style.rotate = '-180deg'
+    }
+
+    if (arrow3.style.transition == '0.5s') {
+        arrow3.style.transition = '0'
+    } else {
+        arrow3.style.transition = '0.5s'
+    }
+}
+
+function showDesc4() {
+    if (task__desc_4.style.display == 'none') {
+        task__desc_4.style.display = 'block'
+    } else {
+        task__desc_4.style.display = 'none'
+    }
+
+
+
+    if (arrow4.style.rotate == '-180deg') {
+        arrow4.style.rotate = '0deg'
+    } else {
+        arrow4.style.rotate = '-180deg'
+    }
+
+    if (arrow4.style.transition == '0.5s') {
+        arrow4.style.transition = '0'
+    } else {
+        arrow4.style.transition = '0.5s'
+    }
+}
+
+function showDesc5() {
+    if (task__desc_5.style.display == 'none') {
+        task__desc_5.style.display = 'block'
+    } else {
+        task__desc_5.style.display = 'none'
+    }
+
+
+
+    if (arrow5.style.rotate == '-180deg') {
+        arrow5.style.rotate = '0deg'
+    } else {
+        arrow5.style.rotate = '-180deg'
+    }
+
+    if (arrow5.style.transition == '0.5s') {
+        arrow5.style.transition = '0'
+    } else {
+        arrow5.style.transition = '0.5s'
+    }
+}
+
+function showDesc6() {
+    if (task__desc_6.style.display == 'none') {
+        task__desc_6.style.display = 'block'
+    } else {
+        task__desc_6.style.display = 'none'
+    }
+
+
+
+    if (arrow6.style.rotate == '-180deg') {
+        arrow6.style.rotate = '0deg'
+    } else {
+        arrow6.style.rotate = '-180deg'
+    }
+
+    if (arrow6.style.transition == '0.5s') {
+        arrow6.style.transition = '0'
+    } else {
+        arrow6.style.transition = '0.5s'
+    }
+}
